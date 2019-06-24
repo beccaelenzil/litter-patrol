ddfdd145d40fce8bcb3d656e76627656f4597d77
Kaida Masaki <kaida@adadevelopersacademy.org>
2019-06-24 13:23:33 -0700

    Cleanup style in src/App.js to reduce diff noise

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
   config = {
     itemTypes: {
       // type: spawn rate (weighting)
-      litter:  20,
-      rock:     5,
-      bush:     5,
-      flower:   5,
+      litter: 20,
+      rock: 5,
+      bush: 5,
+      flower: 5,
       mushroom: 5,
     },
     spawnRate: 1.2, // Hz
@@ -30,8 +30,8 @@ class App extends Component {
     };
 
     // Uncomment this to spawn a single test item
-    //const testItem = this.spawnItem(Date.now());
-    //this.state.items.push(testItem);
+    // const testItem = this.spawnItem(Date.now());
+    // this.state.items.push(testItem);
 
     // Uncomment this to automatically spawn new items
     this.enableSpawner();
@@ -57,13 +57,13 @@ class App extends Component {
     return (
       <div className="game">
         <section className="hud">
-          <h2 className="score">Litter Spotted: { this.state.points }</h2>
+          <h2 className="score">Litter Spotted: {this.state.points}</h2>
           <img className="logo" src={logo} alt="Litter Patrol logo" />
         </section>
 
         <section className="level">
-          { this.levelBackground() }
-          { items }
+          {this.levelBackground()}
+          {items}
         </section>
 
       </div>
@@ -82,22 +82,22 @@ class App extends Component {
       return item.expiration !== null && item.expiration > time;
     });
 
-    if(items.length !== this.state.items.length) {
+    if (items.length !== this.state.items.length) {
       newState.items = items;
     }
 
     // Should we spawn a new item?
-    const {spawnRate, spawnRateRnd} = this.config;
-    if(this.spawnItems && spawnRate > 0) {
+    const { spawnRate, spawnRateRnd } = this.config;
+    if (this.spawnItems && spawnRate > 0) {
       let spawnDelta = time - (this.lastSpawn || 0);
 
       // Randomize spawn rate
-      if(spawnRateRnd > 0) {
+      if (spawnRateRnd > 0) {
         const factor = 1 + Math.random() * spawnRateRnd;
         spawnDelta *= factor;
       }
 
-      if(spawnDelta >= (1 / spawnRate) * 1000) {
+      if (spawnDelta >= (1 / spawnRate) * 1000) {
         newState.items = [
           ...(newState.items || this.state.items),
           this.spawnItem(time),
@@ -118,12 +118,13 @@ class App extends Component {
     const expiration = time + this.config.itemLifetime;
     const height = Math.random() * this.config.spawnHeight + this.config.spawnFloor;
 
-    return {id, type, expiration, height};
+    return { id, type, expiration, height };
   }
 
   randomType() {
     // Figure out the total of all the weighted types
-    const totalWeight = Object.values(this.config.itemTypes).reduce((l,r)=>l+r,0);
+    const weights = Object.values(this.config.itemTypes)
+    const totalWeight = weights.reduce((sum, weight) => sum + weight);
 
     // Get a random value between zero and the total
     let choice = Math.random() * totalWeight;
@@ -131,10 +132,10 @@ class App extends Component {
 
     // Loop through all item types and figure out which one we chose
     Object.entries(this.config.itemTypes).forEach(([type, weight]) => {
-      if(selectedType !== null) return; // We've already found our choice
+      if (selectedType !== null) return; // We've already found our choice
 
       // If the random value was less than this type's weight
-      if(choice <= weight) {
+      if (choice <= weight) {
         selectedType = type; // then we've selected it
       } else {
         choice -= weight; // otherwise move past this entry
@@ -149,8 +150,19 @@ class App extends Component {
   }
 
   levelBackground() {
-    const layers = ['clouds-1', 'clouds-2', 'clouds-3', 'clouds-4',
-                    'hills-1','hills-2','bushes','trees-1','trees-2','ground'];
+    const layers = [
+      'clouds-1',
+      'clouds-2',
+      'clouds-3',
+      'clouds-4',
+      'hills-1',
+      'hills-2',
+      'bushes',
+      'trees-1',
+      'trees-2',
+      'ground'
+    ];
+    
     return (
       <div className="level-bg">
         {layers.map(layer => (<div className={`level-bg-${layer}`} key={layer} />))}
@@ -167,7 +179,7 @@ class App extends Component {
 
       const newState = this.tick(Date.now());
 
-      if(Object.keys(newState).length > 0) {
+      if (Object.keys(newState).length > 0) {
         this.setState(newState, callback);
       } else {
         callback();
@@ -178,7 +190,7 @@ class App extends Component {
   }
 
   componentWillUnmount() {
-    if(this.updateTimer !== null) {
+    if (this.updateTimer !== null) {
       clearInterval(this.updateTimer);
     }
   }
